e085e4fc39f2db42e7f4f047700ccb1e4d9a0233
zppppppx <zppppppx@gmail.com>
2023-05-16 17:30:31 -0700

    Added decoder layer

--- models/transformer.py
+++ models/transformer.py
@@ -18,7 +18,7 @@ class Transformer(nn.Module):
         self.memory_length = memory_length
 
 class EncoderLayer(nn.Module):
-    def __init__(self, dmodel, nhead, dim_ffn=512, dropout=0.1, activation=F.relu, temporal_encoding=True) -> None:
+    def __init__(self, dmodel, nhead, dim_ffn=512, dropout=0.1, activation=F.relu) -> None:
         super(EncoderLayer, self).__init__()
         self.self_attn = nn.MultiheadAttention(dmodel, nhead, dropout=dropout)
 
@@ -38,16 +38,16 @@ class EncoderLayer(nn.Module):
         # Temporal encoding
         self.temporal_encoder = TemporalEncoding()
 
-    def with_pos_embed(self, tensor, pos=None):
-        return tensor if pos is None else tensor + pos
+    def with_temp_embed(self, tensor, temp=None):
+        return tensor if temp is None else tensor + temp
 
     def forward(self, 
                 src,
                 src_mask = None,
                 src_key_padding_mask = None,
-                pos = None
+                temp = None
                 ):
-        q = k = self.with_pos_embed(src, pos)
+        q = k = self.with_temp_embed(src, temp)
         src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
         
@@ -63,6 +63,56 @@ class EncoderLayer(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, dmodel) -> None:
+    def __init__(self, dmodel, nhead, dim_ffn=512, dropout=0.1, activation=F.relu) -> None:
         super(DecoderLayer, self).__init__()
-        self.dmodel = dmodel
+        self.self_attn = nn.MultiheadAttention(dmodel, nhead, dropout)
+        self.cross_attn = nn.MultiheadAttention(dmodel, nhead, dropout)
+
+        # FFN
+        self.linear1 = nn.Linear(dmodel, dim_ffn)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_ffn, dmodel)
+
+        self.norm1 = nn.LayerNorm(dmodel)
+        self.norm2 = nn.LayerNorm(dmodel)
+        self.norm3 = nn.LayerNorm(dmodel)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+        self.activation = activation
+
+    def with_temp_embed(self, tensor, temp=None):
+        return tensor if temp is None else tensor + temp
+    
+    def forward(self, 
+                tgt,                            # the input of the decoder
+                memory,                         # the output of the encoder (also the input of the cross attention block)
+                tgt_mask = None,
+                memory_mask = None,
+                tgt_key_padding_mask = None,
+                memory_key_padding_mask = None,
+                temp = None,                    # temporal encoding of decoder's input
+                temp_memory = None,             # temporal encoding of encoder's output
+                ):
+        q = k = self.with_temp_embed(tgt, temp)
+        tgt2 = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
+                              key_padding_mask=tgt_key_padding_mask)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+
+        tgt2 = self.cross_attn(query=self.with_temp_embed(tgt, temp),
+                               key=self.with_temp_embed(memory, temp_memory),
+                               value = memory, attn_mask=memory_mask,
+                               key_padding_mask = memory_key_padding_mask)[0]
+        
+        # Second add-norm layer
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+
+        # Third add-norm layer
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+
+        return tgt
