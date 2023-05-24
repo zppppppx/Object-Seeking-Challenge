d8ba5de2729286e0d91e45f2caecea0f2d4cdc54
zppppppx <zppppppx@gmail.com>
2023-05-23 22:12:28 -0700

    Added more functions in `w2c` in utils

--- utils/word2vec/w2c.py
+++ utils/word2vec/w2c.py
@@ -1,4 +1,4 @@
-from gensim.models import Word2Vec
+from gensim.models.word2vec import Word2Vec, KeyedVectors
 from os import path
 import os
 
@@ -19,19 +19,42 @@ def preprocess(directory):
 
             lines.pop(0)
             # print(lines)
-            lines = [[line.split('\"')[1]] for line in lines]
+            lines = [line.split('\"')[1].split(' ') for line in lines]
             # print(len(lines))
 
             allwords.extend(lines)
     # print(len(allwords))
     return allwords
     
-def load(model_path):
+def load_model(model_path):
     return Word2Vec.load(model_path)
+
+def load_vector(vector_path):
+    return KeyedVectors.load_word2vec_format(vector_path, )
+
+def similarity(wv, tgt1, tgt2):
+    return wv.similarity(tgt1, tgt2)
 
 
 if __name__ == "__main__":
-    allwords = preprocess(r'F:\grad\quater3\ECE285\Object-Seeking-Challenge\utils\word2vec')
-    print(allwords)
-    wv = Word2Vec(allwords, vector_size=512)
-    wv.save('home_wv.model')
+    # allwords = preprocess(r'F:\grad\quater3\ECE285\Object-Seeking-Challenge\utils\word2vec')
+    # print(allwords)
+    # wv = Word2Vec(allwords, vector_size=512)
+    
+    # wv.save('home_wv.model')
+    # model = Word2Vec.load('home_wv.model')
+    # print(similarity(wv, 'table', 'chair'))
+
+    
+
+    # print(similarity(wv, 'chair', 'table'))
+
+    wv = load_vector(r"F:\grad\quater3\ECE285\Object-Seeking-Challenge\model.txt")
+    print(similarity(wv, 'chair', 'table'))
+    print(wv['chair'])
+    import torch
+
+    wvs = torch.tensor(wv['chair']).view(1, -1)
+    wvs = wvs.repeat(1, 1)
+
+    print(wvs.shape)
